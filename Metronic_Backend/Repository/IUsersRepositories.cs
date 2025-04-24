f309d237b24a2588f239c6311d8d9e50fef2f3bd
Abdul Samad <abdul.sammad@binaryaxon.com>
2025-04-24 11:57:06 +0500

    Add project files.

---
+++ Metronic_Backend/Repository/IUsersRepositories.cs
@@ -0,0 +1,19 @@
+﻿using Metronic_Backend.Models;
+
+namespace Metronic_Backend.Repository
+{
+    public interface IUsersRepositories
+    {
+
+        Task<List<Users>> GetAllUsersAsync();
+
+        Task<List<Users>> GetUsersAsyncById(int id);
+
+        Task<List<Users>> DeleteUsersAsyncById(int id);
+
+        Task<int> AddNewUserRecordAsync(string userName, string userEmail, string userPassword);
+
+        Task<Users?> UpdateUserRecordAsyncById(int id, string userName, string userEmail, string userPassword);
+
+    }
+}
---
+++ Metronic_Backend/Service/UsersService.cs
@@ -0,0 +1,44 @@
+﻿using Metronic_Backend.Models;
+using Metronic_Backend.Repository;
+
+namespace Metronic_Backend.Service
+
+{
+    public class UsersService : IUsersService
+    {
+
+        private readonly IUsersRepositories _usersRepository;
+
+        public UsersService(IUsersRepositories usersRepository)
+        {
+            _usersRepository = usersRepository;
+        }
+
+        public async Task<List<Users>> GetUsersAsync()
+        {
+            return await _usersRepository.GetAllUsersAsync();
+        }
+
+        public async Task<List<Users>> GetUsersAsyncById(int Id)
+        {
+            return await _usersRepository.GetUsersAsyncById(Id);
+        }
+
+        public async Task<List<Users>> DeleteUsersAsyncById(int Id)
+        {
+            return await _usersRepository.DeleteUsersAsyncById(Id);
+        }
+
+        public async Task<bool> AddNewUserRecordAsync(string userName, string userEmail, string userPassword)
+        {
+            int result = await _usersRepository.AddNewUserRecordAsync(userName, userEmail, userPassword);
+            return result > 0;
+        }
+
+        public async Task<Users?> UpdateUserRecordAsyncById(int id, string userName, string userEmail, string userPassword)
+        {
+            return await _usersRepository.UpdateUserRecordAsyncById(id, userName, userEmail, userPassword);
+        }
+
+    }
+}
